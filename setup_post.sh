8ef564574a96177d2c6819db766a7228e4964fb6
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2025-01-04 21:57:42 +0530

    Done

--- setup_post.sh
+++ setup_post.sh
@@ -11,8 +11,3 @@ minikube start --driver=docker
 alias k="minikube kubectl --"
 k get nodes
 
-# helm installation
-sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-sudo chmod 777 get_helm.sh
-./get_helm.sh
-helm
